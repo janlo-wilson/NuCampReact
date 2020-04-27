bdd0615ebd2e13b4330005ce94e2b1465d04a775
Jan Wilson <janlo.wilson@gmail.com>
2020-04-27 09:44:01 -0700

    Functional Components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -19,18 +19,18 @@ class Main extends Component {
     }
 
     render() {
-      return (
-          <div>
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-              <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
-          </div>
-      );
-  }
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">NuCamp</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
+                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+            </div>
+        );
+    }
 }
 
 export default Main;
